48c43d1e39dda6926482faa301e0d7538fbacc11
tangguhriyadi <mtangguh97@gmail.com>
2023-02-14 09:03:30 +0700

    style(works): slicing works component using modal

--- src/components/views/AppHome.tsx
+++ src/components/views/AppHome.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import AppAbout from '../home/AppAbout'
+import AppFeature from '../home/AppFeature'
 import AppHero from '../home/AppHero'
+import AppWorks from '../home/AppWorks'
 
 const AppHome:React.FC = () => {
   return (
     <div className="main">
         <AppHero />
         <AppAbout />
+        <AppFeature />
+        <AppWorks />
     </div>
   )
 }
