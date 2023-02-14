eb092364e70f7576ccd17485f3cce55f113ce0ee
Muhammad Tangguh Riyadi <mtangguh97@gmail.com>
2023-02-14 20:47:15 +0700

    style(footer): create footer component using Backtop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { Layout } from 'antd';
-import React from 'react';
+import { Layout } from "antd";
+import React from "react";
 /* import logo from './logo.svg'; */
-import './App.css';
-import AppHeader from './components/common/AppHeader';
-import AppHome from './components/views/AppHome';
+import "./App.css";
+import AppFooter from "./components/common/AppFooter";
+import AppHeader from "./components/common/AppHeader";
+import AppHome from "./components/views/AppHome";
 
-const {Header, Content} = Layout
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Layout className='mainLayout'>
+    <Layout className="mainLayout">
       <Header>
         <AppHeader />
       </Header>
       <Content>
         <AppHome />
       </Content>
+      <Footer>
+        <AppFooter />
+      </Footer>
     </Layout>
   );
 }
